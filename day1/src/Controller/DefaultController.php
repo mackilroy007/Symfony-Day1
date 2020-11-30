a1096c8202725ddf87671a206781fa609296c2f8
mackilroy007 <mackilroy007@gmail.com>
2020-11-30 14:49:01 +0100

    addind router links and templates

---
+++ day1/src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+// src/Controller/DefaultController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController
+{
+    /**
+    * @Route("/hello/{name}")
+    */
+// public function index()
+public function index($name)
+{
+// return new Response('Hello!');
+return new Response("Hello $name");
+}
+}
