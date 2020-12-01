81bcad9fd8a70ae14055ac2ea0b0b55aa53c310b
mackilroy007 <mackilroy007@gmail.com>
2020-12-01 17:46:16 +0100

    adding the data base display content to the index.html.twig page

--- day1/src/Controller/ProductController.php
+++ day1/src/Controller/ProductController.php
@@ -10,6 +10,19 @@ use Symfony\Component\HttpFoundation\Response;
 class ProductController extends AbstractController
 {
     /**
+     * @Route("/", name="indexAction")
+     */
+    public  function indexAction()
+    {
+        $products = $this->getDoctrine()
+            ->getRepository(Product::class)
+            ->findAll(); // this variable $products will store the result of running a query to find all the products
+        return $this->render('product/index.html.twig', array("products" => $products)); // i send the variable that have all the products as an array of objects to the index.html.twig page
+    }
+
+
+
+    /**
      * @Route("/create", name="createAction")
      */
     public function createAction()
@@ -18,7 +31,7 @@ class ProductController extends AbstractController
         // you can fetch the EntityManager via $this->getDoctrine()
         // or you can add an argument to your action: createAction(EntityManagerInterface $em)
         $em = $this->getDoctrine()->getManager();
-        $product = new  Product(); // here we will create an object from our class Product.
+        $product = new Product(); // here we will create an object from our class Product.
         $product->setName('Keyboard'); // in our Product class we have a set function for each column in our db
         $product->setPrice(19);
 
@@ -27,5 +40,23 @@ class ProductController extends AbstractController
         // actually executes the queries (i.e. the INSERT query)
         $em->flush();
         return  new Response('Saved new product with id ' . $product->getId());
+    }
+
+    /**
+     * @Route("/details/{productId}", name="detailsAction")
+     */
+    public function showdetailsAction($productId)
+    {
+        $product = $this->getDoctrine()
+            ->getRepository(Product::class)
+            ->find($productId);
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id ' . $productId
+            );
+        } else {
+            return new Response('Details from the product with id ' . $productId . ", Product name is " . $product->getName() . " and it cost " . $product->getPrice() . "€");
+        }
+        // ... do something, like pass the $product object into a template
     }
 }
