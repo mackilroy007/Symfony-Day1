43a159b606068df6881ef0e1d37ba09765f4e00d
mackilroy007 <mackilroy007@gmail.com>
2020-12-01 11:36:20 +0100

    var example implemented over the controller section and executed in the templates environment

--- day1/src/Controller/DefaultController.php
+++ day1/src/Controller/DefaultController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\ Controller \AbstractController;
+use Symfony\Component\HttpFoundation\ Request;
 
-class DefaultController
+class DefaultController extends AbstractController
 {
     /**
      * @Route("/hello/{name}")
@@ -23,5 +25,15 @@ class DefaultController
     public function simple()
     {
         return new Response('Simple! Easy! Great!');
+    }
+
+    /**
+     * @Route("/test/variable", name="var")
+     */
+
+    public  function testVarAction(Request $request)
+    {
+        $arr = array("name" => "serri", "age" => 30); // here we create a simple array have keys and values
+        return $this->render('test/test.html.twig', array("varName" => $arr)); // this is the way how to send a variable from php (variable you created in the controller) to the twig file
     }
 }
