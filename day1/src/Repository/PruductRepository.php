a44c72bf8a8d462b56eaefb5a1c1d9be7ba8a9f0
mackilroy007 <mackilroy007@gmail.com>
2020-12-01 14:34:38 +0100

    generating new entitiy by creating a dbase within the .env environment

---
+++ day1/src/Repository/PruductRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Pruduct;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Pruduct|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Pruduct|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Pruduct[]    findAll()
+ * @method Pruduct[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class PruductRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Pruduct::class);
+    }
+
+    // /**
+    //  * @return Pruduct[] Returns an array of Pruduct objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('p.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Pruduct
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
